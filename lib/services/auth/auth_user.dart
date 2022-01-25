6bf3fc25e429143c854c2890870c71826fd88623
amina shiyana <miscshiyana@gmail.com>
2022-01-26 01:18:16 +0300

    unit test done

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,8 +5,8 @@ import 'package:flutter/foundation.dart';
 class AuthUser {
   final bool isEmailVerified;
 
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
-  factory AuthUser.fromFirebase(User user) =>
-      AuthUser(user.emailVerified); //created a factory constructor
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+      isEmailVerified: user.emailVerified); //created a factory constructor
 }
